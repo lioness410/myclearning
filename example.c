98027687c66eb2d4835e5078842567e36d511ef2
lioness410 <wadiya.pooja0123@gmail.com>
2022-08-16 13:23:21 +0200

    calculate the percentage of 5 subjects

--- example.c
+++ example.c
@@ -1,24 +1,19 @@
 #include<stdio.h>
+int calpercentage(int sub1,int sub2,int sub3,int sub4,int sub5);
 
 int main()
 {
-  int grosspayment,basic,da,hra,da1,hra1;
+  int sub1= 78;
+  int sub2= 89;
+  int sub3= 67;
+  int sub4= 91;
+  int sub5= 88;
 
-  printf("enter basic salary:");
-  scanf("%d",&basic);
+   printf("percentage is:%d",calpercentage(sub1,sub2,sub3,sub4,sub5));
 
-  printf("enter DA:");
-  scanf("%d",&da1);
-
-  printf("enter HRA");
-  scanf("%d",&hra1);
-
-  da = (da1*basic)/100;
-  hra = (hra1*basic)/100;
-  
-  grosspayment = basic+da+hra;
-  printf("grosssalary:%d\n",grosspayment);
-  
-
-    return 0;
+   return 0;
+}
+int calpercentage(int sub1,int sub2,int sub3,int sub4,int sub5)
+{
+   return(sub1+sub2+sub3+sub4+sub5)/5;
 }
