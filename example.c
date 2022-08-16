544fd9f0ede7748f7a40ef95f2a635d8e6ed2cb4
lioness410 <wadiya.pooja0123@gmail.com>
2022-08-16 13:34:06 +0200

    convert temperature celsius into fahrenheit

--- example.c
+++ example.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
-int calpercentage(int sub1,int sub2,int sub3,int sub4,int sub5);
+
 
 int main()
 {
-  int sub1= 78;
-  int sub2= 89;
-  int sub3= 67;
-  int sub4= 91;
-  int sub5= 88;
+   float cel;
+   float fah;
 
-   printf("percentage is:%d",calpercentage(sub1,sub2,sub3,sub4,sub5));
+   printf("enter temperature in celsius:");
+   scanf("%f",&cel);
 
+   fah =(1.8*cel)+32;
+   printf("temperature in fahrenheit:%f",fah);
+   
    return 0;
-}
-int calpercentage(int sub1,int sub2,int sub3,int sub4,int sub5)
-{
-   return(sub1+sub2+sub3+sub4+sub5)/5;
 }
