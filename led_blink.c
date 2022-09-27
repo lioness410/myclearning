a23af556642bf58135f5f23590fa73547dfffd6c
lioness410 <wadiya.pooja0123@gmail.com>
2022-09-27 12:52:31 +0200

    LED sequce reverse

--- led_blink.c
+++ led_blink.c
@@ -50,5 +50,50 @@ int main() {
         printf("Blink! LED is now %d\n", led4.read());
 
         wait_ms(500);
+        
+         led4 = !led4;
+        if(led4)
+        {
+            led1=0;
+            led2=0;
+            led3=0;
+        }
+        printf("Blink! LED is now %d\n", led4.read());
+
+        wait_ms(500);
+        
+         led3 = !led3;
+        if(led3)
+        {
+            led1=0;
+            led2=0;
+            led4=0;
+        }
+        printf("Blink! LED is now %d\n", led3.read());
+
+        wait_ms(500);
+        
+        led2 = !led2;
+        if(led2)
+        {
+            led1=0;
+            led3=0;
+            led4=0;
+        }
+        printf("Blink! LED is now %d\n", led2.read());
+
+        wait_ms(500);
+        
+        led1 = !led1;
+        if(led1)
+        {
+            led2=0;
+            led3=0;
+            led4=0;
+        }
+        printf("Blink! LED is now %d\n", led1.read());
+
+        wait_ms(500);
     }
 }
+
