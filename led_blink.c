88f9f52eb9f4a02f82d9ccdf6425da683c082eaf
lioness410 <wadiya.pooja0123@gmail.com>
2022-09-27 15:10:45 +0200

    LED

--- led_blink.c
+++ led_blink.c
@@ -1,3 +1,4 @@
+
 #include "mbed.h"
 
 DigitalOut led1(LED1);
@@ -5,95 +6,82 @@ DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
 
-int main() {
-    while (1) {
-        led1 = !led1;
-        if(led1)
-        {
-            led2=0;
-            led3=0;
-            led4=0;
-        }
-        printf("Blink! LED is now %d\n", led1.read());
+void led1_toggle(void);
+void led2_toggle(void);
+void led3_toggle(void);
+void led4_toggle(void);
 
-        wait_ms(500);
-        
-        led2 = !led2;
-        if(led2)
-        {
-            led1=0;
-            led3=0;
-            led4=0;
-        }
-        printf("Blink! LED is now %d\n", led2.read());
-
-        wait_ms(500);
-        
-        led3 = !led3;
-        if(led3)
-        {
-            led1=0;
-            led2=0;
-            led4=0;
-        }
-        printf("Blink! LED is now %d\n", led3.read());
-
-        wait_ms(500);
-        
-        led4 = !led4;
-        if(led4)
-        {
-            led1=0;
-            led2=0;
-            led3=0;
-        }
-        printf("Blink! LED is now %d\n", led4.read());
-
-        wait_ms(500);
-        
-         led4 = !led4;
-        if(led4)
-        {
-            led1=0;
-            led2=0;
-            led3=0;
-        }
-        printf("Blink! LED is now %d\n", led4.read());
-
-        wait_ms(500);
-        
-         led3 = !led3;
-        if(led3)
-        {
-            led1=0;
-            led2=0;
-            led4=0;
-        }
-        printf("Blink! LED is now %d\n", led3.read());
-
-        wait_ms(500);
-        
-        led2 = !led2;
-        if(led2)
-        {
-            led1=0;
-            led3=0;
-            led4=0;
-        }
-        printf("Blink! LED is now %d\n", led2.read());
-
-        wait_ms(500);
-        
-        led1 = !led1;
-        if(led1)
-        {
-            led2=0;
-            led3=0;
-            led4=0;
-        }
-        printf("Blink! LED is now %d\n", led1.read());
-
-        wait_ms(500);
+int main()
+{
+    while(1)
+    {
+        led1_toggle();
+        led2_toggle();
+        led3_toggle();
+        led4_toggle();
+        led4_toggle();
+        led3_toggle();
+        led2_toggle();
+        led1_toggle();
     }
 }
+void led1_toggle(void)
+{
+    led1 = !led1;
+    if(led1)
+    {
+        led2=0;
+        led3=0;
+        led4=0;
+    }
+    printf("Blink! LED is now:%d\n",led1.read());
+    
+    wait_ms(500);
+}
+void led2_toggle(void)
+{
+    led2 = !led2;
+    if(led2)
+    {
+        led1=0;
+        led3=0;
+        led4=0;
+    }
+    printf("Blink! LED is now:%d\n",led2.read());
+    
+    wait_ms(500);
+} 
+void led3_toggle(void)
+{
+    led3 = !led3;
+    if(led3)
+    {
+        led1=0;
+        led2=0;
+        led4=0;
+    }
+    printf("Blink! LED is now:%d\n",led3.read());
+    
+    wait_ms(500);
+}
+void led4_toggle(void)
+{
+    led4 = !led4;
+    if(led4)
+    {
+        led1=0;
+        led2=0;
+        led3=0;
+    }
+    printf("Blink! LED is now:%d\n",led4.read());
+    
+    wait_ms(500);
+}
+    
+    
+    
+    
+    
+    
+    
 
