d480823663aa4b13184e159f8df4bc099d3559ad
lioness410 <wadiya.pooja0123@gmail.com>
2022-09-28 10:50:54 +0200

    led1 and 3 blink

--- led_blink.c
+++ led_blink.c
@@ -1,4 +1,3 @@
-
 #include "mbed.h"
 
 DigitalOut led1(LED1);
@@ -28,60 +27,13 @@ int main()
 void led1_toggle(void)
 {
     led1 = !led1;
+    led3 = !led3;
+    
     if(led1)
     {
         led2=0;
-        led3=0;
+        led3=1;
         led4=0;
     }
     printf("Blink! LED is now:%d\n",led1.read());
-    
-    wait_ms(500);
-}
-void led2_toggle(void)
-{
-    led2 = !led2;
-    if(led2)
-    {
-        led1=0;
-        led3=0;
-        led4=0;
-    }
-    printf("Blink! LED is now:%d\n",led2.read());
-    
-    wait_ms(500);
-} 
-void led3_toggle(void)
-{
-    led3 = !led3;
-    if(led3)
-    {
-        led1=0;
-        led2=0;
-        led4=0;
-    }
     printf("Blink! LED is now:%d\n",led3.read());
-    
-    wait_ms(500);
-}
-void led4_toggle(void)
-{
-    led4 = !led4;
-    if(led4)
-    {
-        led1=0;
-        led2=0;
-        led3=0;
-    }
-    printf("Blink! LED is now:%d\n",led4.read());
-    
-    wait_ms(500);
-}
-    
-    
-    
-    
-    
-    
-    
-
